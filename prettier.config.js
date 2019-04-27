1fdf561f33b968392d5855b73f990d4387c59c5b
bitttttten <bitttttten@googlemail.com>
2019-04-27 20:52:37 +0200

    init 🎉

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: false,
+    jsxSingleQuote: true,
+    singleQuote: true,
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { TText } from "./types"
+
+const message: TText = 'the world never says hello back'
+
+console.log(message)
