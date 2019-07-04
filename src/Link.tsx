b03336b34b651ad47224ec94bb4d6271f0b625f1
bitttttten <bitttttten@googlemail.com>
2019-07-04 14:13:45 +0200

    feature: allow browser handling of clicks on special occasions

--- src/Link.tsx
+++ src/Link.tsx
@@ -12,11 +12,21 @@ export function Link({
 }: ILinkProps) {
     const { goTo, getHref } = useOatmilk()
 
-	const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
-        e.preventDefault()
-        goTo(routeName, state, queryParams)
+    const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
         if (typeof onClickFromUser === 'function') {
             onClickFromUser(e)
+        }
+        if (
+            e.button === 0 && // ignore right clicks
+            !props.target && // let browser handle "target=_blank"
+            !e.defaultPrevented && // onClick prevented default
+            !e.metaKey && // ignore clicks with modifier keys...
+            !e.altKey &&
+            !e.ctrlKey &&
+            !e.shiftKey
+        ) {
+            e.preventDefault()
+            goTo(routeName, state, queryParams)
         }
     }
     const href = getHref(routeName, state, queryParams)
