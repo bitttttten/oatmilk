2bdc8ed4450c1dd9f938eee87dce16ff157cd4ca
bitttttten <bitttttten@googlemail.com>
2019-07-08 10:51:19 +0200

    feature: forward props on link element

--- src/Link.tsx
+++ src/Link.tsx
@@ -1,39 +1,46 @@
-import React, { MouseEvent } from 'react'
+import React, { forwardRef, MouseEvent } from 'react'
 import { useOatmilk } from './hooks'
 import { ILinkProps } from './types'
 
-export function Link({
-    onClick: onClickFromUser,
-    children,
-    queryParams,
-    routeName,
-    state,
-    ...props
-}: ILinkProps) {
-    const { goTo, getHref } = useOatmilk()
+const Link = forwardRef<HTMLAnchorElement, ILinkProps>(
+    (
+        {
+            onClick: onClickFromUser,
+            children,
+            queryParams,
+            routeName,
+            state,
+            ...props
+        },
+        ref,
+    ) => {
+        const { goTo, getHref } = useOatmilk()
 
-    const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
-        if (typeof onClickFromUser === 'function') {
-            onClickFromUser(e)
+        const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
+            if (typeof onClickFromUser === 'function') {
+                onClickFromUser(e)
+            }
+            if (
+                e.button === 0 && // ignore right clicks
+                !props.target && // let browser handle "target=_blank"
+                !e.defaultPrevented && // onClick prevented default
+                !e.metaKey && // ignore clicks with modifier keys...
+                !e.altKey &&
+                !e.ctrlKey &&
+                !e.shiftKey
+            ) {
+                e.preventDefault()
+                goTo(routeName, state, queryParams)
+            }
         }
-        if (
-            e.button === 0 && // ignore right clicks
-            !props.target && // let browser handle "target=_blank"
-            !e.defaultPrevented && // onClick prevented default
-            !e.metaKey && // ignore clicks with modifier keys...
-            !e.altKey &&
-            !e.ctrlKey &&
-            !e.shiftKey
-        ) {
-            e.preventDefault()
-            goTo(routeName, state, queryParams)
-        }
-    }
-    const href = getHref(routeName, state, queryParams)
+        const href = getHref(routeName, state, queryParams)
 
-    return (
-        <a {...props} href={href} onClick={onClick}>
-            {children}
-        </a>
-    )
-}
+        return (
+            <a {...props} href={href} ref={ref} onClick={onClick}>
+                {children}
+            </a>
+        )
+    },
+)
+
+export { Link }
