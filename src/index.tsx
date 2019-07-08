fede34eef691f7ae6f890846981a0d3b60570d2b
bitttttten <bitttttten@googlemail.com>
2019-07-08 10:32:44 +0200

    chore: export getRouteByName

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 export { Provider, Context } from './Router'
-export { getMatchFromUrl, getMatchWithCalleeFromUrl } from './utils'
+export { getMatchFromUrl, getMatchWithCalleeFromUrl, getRouteByName } from './utils'
 export { Link } from './Link'
 export { RouterView } from './RouterView'
 export { useOatmilk } from './hooks'
